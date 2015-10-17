b6199b7138a4d3fd1fd3a4de2ee45f091e6868ea
Jiang Long <jianglong@startimes.com.cn>
2015-10-17 23:43:41 +0800

    Modify build tool from maven to gradle

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git pull
-mvn -DskipTests clean package
-rm -rf /home/virtual_hosts/plantuml.mvnsearch.org/ROOT
-cp -r target/plantuml-gist /home/virtual_hosts/plantuml.mvnsearch.org/ROOT
